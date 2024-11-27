2baf524ca147ea65a8da9412040f5569c3131fae
Nicolas Frere <107085649+Gotlub@users.noreply.github.com>
2024-11-27 20:25:31 +0100

    Create correctionProcessus.c

---
+++ semaine2/correctionProcessus.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+
+int main(void)
+{
+    int f1,f2,w1,w2,r1,r2,r3;
+    float calcul;
+    f1=fork();
+    // si f1 == 0 alors on est dans le fils du fork f1
+    if(f1==0)
+    {
+        int res;
+        res=1+5; // Calcul intermédiaire du fils 1
+        printf("Fils1 : calcul terminé. pid : %d \n", getpid());
+        exit(res);
+    }
+    f2=fork();
+    // si f2 == 0 alors on est dans le fils du fork f2
+    if(f2==0)
+    {
+        int res;
+        res=6-2; // Calcul intermédiaire du fils 2
+        printf("Fils2 : calcul terminé. pid : %d \n", getpid());
+        exit(res);
+    }
+    // Si status n'est pas un pointeur nul, le status du processus fils (valeur retournée par exit())
+    // est mémorisé à l'emplacement pointé par status, qui contient si le processus c'est terminé noramllement ou non, ça cause et sa valeur de sortie.
+    // retourne le pid du processus fils si le retour est dû à la terminaison d'un processus fils
+    w1=wait(&r1);
+    w2=wait(&r2);
+    //(si WIFEXITED (status) renvoie vrai) renvoie le code de retour du processus fils passé à
+    // _exit() ou exit() ou la valeur retournée par la fonction main() 
+    r1=WEXITSTATUS(r1);
+    r2=WEXITSTATUS(r2);
+    //w1 est la valleur d'un processus arreté (enfent f1), elle ne peu avoir du processus père (qui est getpid() dans f1!=0)
+    //on ne rentre pas dans cette condition
+    if(w1!=f1)
+    {
+        printf("Pere : en calcul. pid : %d \n", getpid());
+        r3=r1;
+        r1=r2;
+        r2=r3;
+    }
+    r3=8+6; // Calcul intermédiaire du père
+    calcul=(float)(r1*(r2+1))/(float)r3;
+    printf("Résultat du calcul : %f\n",calcul);
+}
